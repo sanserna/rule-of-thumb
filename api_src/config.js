9ed66e39f672956117427169916e0be0df88723f
Santiago Serna <sanserve91@gmail.com>
2019-03-25 12:41:29 -0500

    first project bootstraping

---
+++ api_src/config.js
@@ -0,0 +1,13 @@
+const env = process.env.NODE_ENV || 'development';
+
+export default {
+  env,
+  isDev: env === 'development',
+  isTest: env === 'testing',
+  port: 3000,
+  secrets: {
+    jwt: process.env.JWT_SECRET,
+    jwtExp: '100d'
+  },
+  dbUrl: 'mongodb://localhost:27017/rule-of-thumb'
+};
---
+++ api_src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { json, urlencoded } from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import config from './config';
+import { connect } from './utils/db';
+
+export const app = express();
+
+app.disable('x-powered-by');
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+export const start = async () => {
+  try {
+    await connect();
+    app.listen(config.port, () => {
+      console.log(`REST API on http://localhost:${config.port}/api`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
