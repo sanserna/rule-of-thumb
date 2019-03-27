93ead5d0725f28d539e3884b66203a99b244a4fa
Santiago Serna <sanserve91@gmail.com>
2019-03-26 20:41:31 -0500

    update config

--- api_src/config.js
+++ api_src/config.js
@@ -7,7 +7,7 @@ export default {
   env,
   isDev: env === 'development',
   isTest: env === 'testing',
-  port: 3000,
+  port: process.env.PORT || 3000,
   secrets: {
     jwt: process.env.JWT_SECRET,
     jwtExp: '100d'
